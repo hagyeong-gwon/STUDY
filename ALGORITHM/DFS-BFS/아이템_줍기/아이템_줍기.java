988130e6736f38f423b84570f7f115483b0236bf
hagyeong-gwon <silk3278@gmail.com>
2024-01-12 01:04:01 +0900

    CHG: 아이템_줍기.java

--- ALGORITHM/DFS-BFS/아이템_줍기/아이템_줍기.java
+++ ALGORITHM/DFS-BFS/아이템_줍기/아이템_줍기.java
@@ -1,13 +1,94 @@
+package DFS;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
-class Solution {
-  public static void main(String[] args) {
+public class CoinGet {
+  public static void main(String[] args) throws IOException {
     int[][] rectangle = new int[][]{{1, 1, 7, 4}, {3, 2, 5, 5}, {4, 3, 6, 9}, {2, 6, 8, 8}};
+    int characterX = 1;
+    int characterY = 3;
+    int itemX = 7;
+    int itemY = 8;
 
-    System.out.print(solution(rectangle, 1, 3, 7, 8));
+    int[][] fullMap = new int[50][50];
+
+    for (int i = 0; i < fullMap.length; i++) {
+      Arrays.fill(fullMap[i], 0);
+    }
+
+    for (int i = 0; i < rectangle.length; i++) {
+
+      int maxX = rectangle[i][2] * 2;
+      int maxY = rectangle[i][3] * 2;
+      int[] position = {rectangle[i][0] * 2, rectangle[i][1] * 2, maxX, maxY};
+      System.out.println(Arrays.toString(position));
+      Arrays.fill(fullMap[rectangle[i][1] * 2], rectangle[i][0] * 2, maxX, 1);
+      Arrays.fill(fullMap[maxY], rectangle[i][0] * 2, maxX, 1);
+
+      for (int x = rectangle[i][0] * 2; x < maxX + 1; x++) {
+        int y = rectangle[i][1] * 2;
+        fullMap[y][x] = 1;
+        fullMap[maxY][x] = 1;
+
+        for (int j = 0; j < rectangle.length; j++) {
+          if (j == i) continue;
+
+          if (rectangle[j][0]  * 2 < x && rectangle[j][2]  * 2 > x) {
+            if (rectangle[j][1]  * 2< y && rectangle[j][3]  * 2 > y) {
+              fullMap[y][x] = 0;
+            }
+            if (rectangle[j][1] * 2 < maxY && rectangle[j][3] * 2 > maxY) {
+              fullMap[maxY][x] = 0;
+            }
+          }
+        }
+      }
+
+      for (int y = rectangle[i][1] * 2; y < maxY + 1; y++) {
+        int x = rectangle[i][0] * 2;
+
+        fullMap[y][x] = 1;
+        fullMap[y][maxX] = 1;
+
+
+
+        for (int j = 0; j < rectangle.length; j++) {
+          if (j == i) continue;
+          if (rectangle[j][1]  * 2 < y && rectangle[j][3]  * 2 > y) {
+            if (rectangle[j][0]  * 2 < x && rectangle[j][2]  * 2 > x) {
+              fullMap[y][x] = 0;
+            }
+            if (rectangle[j][0] * 2 < maxX && rectangle[j][2] * 2 > maxX) {
+              fullMap[y][maxX] = 0;
+            }
+          }
+        }
+      }
+    }
+
+//    String path = System.getProperty("user.dir") + "\\sampleSquare.txt";
+    File file = new File("./sampleSquare.txt");
+    if (!file.exists()) {
+      file.createNewFile();
+    }
+    FileWriter fw = new FileWriter(file);
+    BufferedWriter writer = new BufferedWriter(fw);
+
+    for (int i = 0; i < fullMap.length; i++) {
+      int [] a = fullMap[i];
+      System.out.println(Arrays.toString(a).replace("0", "■").replace("1", "□").replaceAll("\\[|\\]",""));
+
+      writer.write(Arrays.toString(a).replace("0", "■").replace("1", "□").replaceAll("\\[|\\]","") + '\n');
+
+    }
+    System.out.println(solution(fullMap, characterX * 2, characterY * 2, itemX * 2, itemY * 2) / 2);
+
   }
-
   public static int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+    System.out.println(characterX+ "|" +characterY+","+itemX+"|"+itemY);
     int answer = 0;
     int result = 0;
 
@@ -21,9 +102,9 @@ class Solution {
     int[][] calc = new int[][]{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     while (!queue.isEmpty()) {
       int queueSize = queue.size();
+
       for (int r = 0; r < queueSize; r++) {
         int[] position = queue.poll();
-        System.out.println(Arrays.toString(position) + "|" + result);
 
         int x = position[0];
         int y = position[1];
@@ -31,30 +112,17 @@ class Solution {
         if (x == itemX && y == itemY) {
           return result;
         }
+
         for (int i = 0; i < calc.length; i++) {
           int[] calcXY = new int[]{x + calc[i][0], y + calc[i][1]};
 
-          boolean isRight = false;
-          boolean isInclude = false;
+          if (rectangle[calcXY[1]][calcXY[0]] == 1 && !visited.containsKey(calcXY[0] + "|" + calcXY[1]) ){
+            visited.put(x + "|" + y, true);
 
-          for (int j = 0; j < rectangle.length; j++) {
-            if ((rectangle[j][0] == calcXY[0] || rectangle[j][2] == calcXY[0]) && rectangle[j][1] <= calcXY[1] && rectangle[j][3] >= calcXY[1]) {
-              isRight = !isInclude && true;
-              continue;
-            } else if ((rectangle[j][1] == calcXY[1] || rectangle[j][3] == calcXY[1]) && rectangle[j][0] <= calcXY[0] && rectangle[j][2] >= calcXY[0]) {
-              isRight = !isInclude && true;
-              continue;
-            }
-            isInclude = (rectangle[j][1] < calcXY[1] && rectangle[j][3] > calcXY[1] && rectangle[j][0] < calcXY[0] && rectangle[j][2] > calcXY[0]);
-            if ((rectangle[j][1] < calcXY[1] && rectangle[j][3] > calcXY[1] && rectangle[j][0] < calcXY[0] && rectangle[j][2] > calcXY[0])) {
-              isInclude = true;
-              if (isRight) isRight = false;
-            }
-          }
-
-          if (isRight && !visited.containsKey(calcXY[0] + "|" + calcXY[1])) {
             queue.add(calcXY);
-            visited.put(calcXY[0] + "|" + calcXY[1], true);
+            System.out.println(Arrays.toString(calcXY) + "|" + result);
+            System.out.println(rectangle[calcXY[1]][calcXY[0]]);
+
           }
         }
       }
